9bd6b7811e4322be428d5d2557158f33d7132217
virophagesp <virophagesp4444@gmail.com>
2026-02-06 11:13:40 +1300

    removed -G option (forgot to before)

--- keyhunt.cpp
+++ keyhunt.cpp
@@ -215,7 +215,7 @@ int main(int argc, char **argv)	{
 
 	printf("[+] Version %s, developed by AlbertoBSD\n",version);
 
-	while ((c = getopt(argc, argv, "b:E:f:N:n:p:r:s:G:z:")) != -1) {
+	while ((c = getopt(argc, argv, "b:E:f:N:n:p:r:s:z:")) != -1) {
 		switch(c) {
 			case 'b':
 				bitrange = strtol(optarg,NULL,10);
