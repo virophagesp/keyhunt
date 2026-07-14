55018be560322037f00acb7cb6e06294e8dcf279
virophagesp <virophagesp4444@gmail.com>
2026-07-14 12:18:12 +1200

    reformatted functions to use tabs instead of spaces

--- keyhunt.cpp
+++ keyhunt.cpp
@@ -136,149 +136,153 @@ Point AddDirect(Point &p1, Point &p2);
 Point DoubleDirect(Point &p);
 
 Point ComputePublicKey(Point *secp, Int *privKey) {
-  int i = 0;
-  uint8_t b;
-  Point Q;
-  Q.Clear();
-  // Search first significant byte
-  for (i = 0; i < 32; i++) {
-    b = privKey->GetByte(i);
-    if(b)
-      break;
-  }
-  Q.Set(secp[256 * i + (b-1)]);
-  i++;
+	int i = 0;
+	uint8_t b;
+	Point Q;
+	Q.Clear();
+	// Search first significant byte
+	for (i = 0; i < 32; i++) {
+		b = privKey->GetByte(i);
+		if(b)
+			break;
+	}
+	Q.Set(secp[256 * i + (b-1)]);
+	i++;
 
-  for(; i < 32; i++) {
-    b = privKey->GetByte(i);
-    if(b)
-      Q.Set2(Add2(Q, secp[256 * i + (b-1)]));
-  }
-  Q.Reduce();
-  return Q;
+	for(; i < 32; i++) {
+		b = privKey->GetByte(i);
+		if(b)
+			Q.Set2(Add2(Q, secp[256 * i + (b-1)]));
+	}
+	Q.Reduce();
+	return Q;
 }
 
 void tohex_dst(char *ptr,char *dst)	{
-  int offset = 0;
-  unsigned char c;
-  for (int i = 0; i <33; i++) {
-    c = ptr[i];
-	sprintf((char*) (dst + offset),"%.2x",c);
-	offset+=2;
-  }
-  dst[66] = 0;
+	int offset = 0;
+	unsigned char c;
+	for (int i = 0; i <33; i++) {
+		c = ptr[i];
+		sprintf((char*) (dst + offset),"%.2x",c);
+		offset+=2;
+	}
+	dst[66] = 0;
 }
 
 void GetPublicKeyHex(Point &pubKey,char *dst){
-  unsigned char publicKeyBytes[65];
-  // Compressed public key
-  publicKeyBytes[0] = pubKey.y.IsEven() ? 0x2 : 0x3;
-  pubKey.x.Get32Bytes(publicKeyBytes + 1);
-  tohex_dst((char*)publicKeyBytes,dst);
+	unsigned char publicKeyBytes[65];
+	// Compressed public key
+	publicKeyBytes[0] = pubKey.y.IsEven() ? 0x2 : 0x3;
+	pubKey.x.Get32Bytes(publicKeyBytes + 1);
+	tohex_dst((char*)publicKeyBytes,dst);
 }
 
 Point AddDirect(Point &p1,Point &p2) {
-  Int _s;
-  Int _p;
-  Int dy;
-  Int dx;
-  Point r;
-  r.z.SetInt32(1);
+	Int _s;
+	Int _p;
+	Int dy;
+	Int dx;
+	Point r;
+	r.z.SetInt32(1);
 
-  dy.ModSub(&p2.y,&p1.y);
-  dx.ModSub(&p2.x,&p1.x);
-  dx.ModInv();
-  _s.ModMulK1(&dy,&dx);     // s = (p2.y-p1.y)*inverse(p2.x-p1.x);
+	dy.ModSub(&p2.y,&p1.y);
+	dx.ModSub(&p2.x,&p1.x);
+	dx.ModInv();
+	// s = (p2.y-p1.y)*inverse(p2.x-p1.x);
+	_s.ModMulK1(&dy,&dx);
 
-  _p.ModSquareK1(&_s);       // _p = pow2(s)
+	// _p = pow2(s)
+	_p.ModSquareK1(&_s);
 
-  r.x.ModSub(&_p,&p1.x);
-  r.x.ModSub(&p2.x);       // rx = pow2(s) - p1.x - p2.x;
+	r.x.ModSub(&_p,&p1.x);
+	// rx = pow2(s) - p1.x - p2.x;
+	r.x.ModSub(&p2.x);
 
-  r.y.ModSub(&p2.x,&r.x);
-  r.y.ModMulK1(&_s);
-  r.y.ModSub(&p2.y);       // ry = - p2.y - s*(ret.x-p2.x);
+	r.y.ModSub(&p2.x,&r.x);
+	r.y.ModMulK1(&_s);
+	// ry = - p2.y - s*(ret.x-p2.x);
+	r.y.ModSub(&p2.y);
 
-  return r;
+	return r;
 }
 
 Point Add2(Point &p1, Point &p2) {
-  // P2.z = 1
-  Int u;
-  Int v;
-  Int u1;
-  Int v1;
-  Int vs2;
-  Int vs3;
-  Int us2;
-  Int a;
-  Int us2w;
-  Int vs2v2;
-  Int vs3u2;
-  Int _2vs2v2;
-  Point r;
-  u1.ModMulK1(&p2.y, &p1.z);
-  v1.ModMulK1(&p2.x, &p1.z);
-  u.ModSub(&u1, &p1.y);
-  v.ModSub(&v1, &p1.x);
-  us2.ModSquareK1(&u);
-  vs2.ModSquareK1(&v);
-  vs3.ModMulK1(&vs2, &v);
-  us2w.ModMulK1(&us2, &p1.z);
-  vs2v2.ModMulK1(&vs2, &p1.x);
-  _2vs2v2.ModAdd(&vs2v2, &vs2v2);
-  a.ModSub(&us2w, &vs3);
-  a.ModSub(&_2vs2v2);
+	// P2.z = 1
+	Int u;
+	Int v;
+	Int u1;
+	Int v1;
+	Int vs2;
+	Int vs3;
+	Int us2;
+	Int a;
+	Int us2w;
+	Int vs2v2;
+	Int vs3u2;
+	Int _2vs2v2;
+	Point r;
+	u1.ModMulK1(&p2.y, &p1.z);
+	v1.ModMulK1(&p2.x, &p1.z);
+	u.ModSub(&u1, &p1.y);
+	v.ModSub(&v1, &p1.x);
+	us2.ModSquareK1(&u);
+	vs2.ModSquareK1(&v);
+	vs3.ModMulK1(&vs2, &v);
+	us2w.ModMulK1(&us2, &p1.z);
+	vs2v2.ModMulK1(&vs2, &p1.x);
+	_2vs2v2.ModAdd(&vs2v2, &vs2v2);
+	a.ModSub(&us2w, &vs3);
+	a.ModSub(&_2vs2v2);
 
-  r.x.ModMulK1(&v, &a);
+	r.x.ModMulK1(&v, &a);
 
-  vs3u2.ModMulK1(&vs3, &p1.y);
-  r.y.ModSub(&vs2v2, &a);
-  r.y.ModMulK1(&r.y, &u);
-  r.y.ModSub(&vs3u2);
+	vs3u2.ModMulK1(&vs3, &p1.y);
+	r.y.ModSub(&vs2v2, &a);
+	r.y.ModMulK1(&r.y, &u);
+	r.y.ModSub(&vs3u2);
 
-  r.z.ModMulK1(&vs3, &p1.z);
+	r.z.ModMulK1(&vs3, &p1.z);
 
-  return r;
+	return r;
 }
 
 Point DoubleDirect(Point &p) {
-  Int _s;
-  Int _p;
-  Int a;
-  Point r;
-  r.z.SetInt32(1);
-  _s.ModMulK1(&p.x,&p.x);
-  _p.ModAdd(&_s,&_s);
-  _p.ModAdd(&_s);
+	Int _s;
+	Int _p;
+	Int a;
+	Point r;
+	r.z.SetInt32(1);
+	_s.ModMulK1(&p.x,&p.x);
+	_p.ModAdd(&_s,&_s);
+	_p.ModAdd(&_s);
 
-  a.ModAdd(&p.y,&p.y);
-  a.ModInv();
-  _s.ModMulK1(&_p,&a);     // s = (3*pow2(p.x))*inverse(2*p.y);
+	a.ModAdd(&p.y,&p.y);
+	a.ModInv();
+	_s.ModMulK1(&_p,&a);     // s = (3*pow2(p.x))*inverse(2*p.y);
 
-  _p.ModMulK1(&_s,&_s);
-  a.ModAdd(&p.x,&p.x);
-  a.ModNeg();
-  r.x.ModAdd(&a,&_p);    // rx = pow2(s) + neg(2*p.x);
+	_p.ModMulK1(&_s,&_s);
+	a.ModAdd(&p.x,&p.x);
+	a.ModNeg();
+	r.x.ModAdd(&a,&_p);    // rx = pow2(s) + neg(2*p.x);
 
-  a.ModSub(&r.x,&p.x);
+	a.ModSub(&r.x,&p.x);
 
-  _p.ModMulK1(&a,&_s);
-  r.y.ModAdd(&_p,&p.y);
-  r.y.ModNeg();           // ry = neg(p.y + s*(ret.x+neg(p.x)));
-  return r;
+	_p.ModMulK1(&a,&_s);
+	r.y.ModAdd(&_p,&p.y);
+	r.y.ModNeg();           // ry = neg(p.y + s*(ret.x+neg(p.x)));
+	return r;
 }
 
 void GetHash160(Point &pubKey, unsigned char *hash) {
-  unsigned char shapk[64];
-  unsigned char publicKeyBytes[128];
+	unsigned char shapk[64];
+	unsigned char publicKeyBytes[128];
 
-  // Compressed public key
-  publicKeyBytes[0] = pubKey.y.IsEven() ? 0x2 : 0x3;
-  pubKey.x.Get32Bytes(publicKeyBytes + 1);
-  sha256_33(publicKeyBytes, shapk);
+	// Compressed public key
+	publicKeyBytes[0] = pubKey.y.IsEven() ? 0x2 : 0x3;
+	pubKey.x.Get32Bytes(publicKeyBytes + 1);
+	sha256_33(publicKeyBytes, shapk);
 
-  ripemd160_32(shapk, hash);
+	ripemd160_32(shapk, hash);
 }
 
 #define KEYBUFFPREFIX(buff,k,fix) \
@@ -299,26 +303,24 @@ void GetHash160(Point &pubKey, unsigned char *hash) {
 (buff)[14] = 0; \
 (buff)[15] = 0x108;
 
-void GetHash160_fromX(unsigned char prefix,
-  Int *k0,Int *k1,Int *k2,Int *k3,
-  uint8_t *h0,uint8_t *h1,uint8_t *h2,uint8_t *h3) {
-  unsigned char sh0[64] __attribute__((aligned(16)));
-  unsigned char sh1[64] __attribute__((aligned(16)));
-  unsigned char sh2[64] __attribute__((aligned(16)));
-  unsigned char sh3[64] __attribute__((aligned(16)));
+void GetHash160_fromX(unsigned char prefix, Int *k0, Int *k1, Int *k2, Int *k3, uint8_t *h0, uint8_t *h1, uint8_t *h2, uint8_t *h3) {
+	unsigned char sh0[64] __attribute__((aligned(16)));
+	unsigned char sh1[64] __attribute__((aligned(16)));
+	unsigned char sh2[64] __attribute__((aligned(16)));
+	unsigned char sh3[64] __attribute__((aligned(16)));
 
-  uint32_t b0[16];
-  uint32_t b1[16];
-  uint32_t b2[16];
-  uint32_t b3[16];
+	uint32_t b0[16];
+	uint32_t b1[16];
+	uint32_t b2[16];
+	uint32_t b3[16];
 
-  KEYBUFFPREFIX(b0, k0, prefix);
-  KEYBUFFPREFIX(b1, k1, prefix);
-  KEYBUFFPREFIX(b2, k2, prefix);
-  KEYBUFFPREFIX(b3, k3, prefix);
+	KEYBUFFPREFIX(b0, k0, prefix);
+	KEYBUFFPREFIX(b1, k1, prefix);
+	KEYBUFFPREFIX(b2, k2, prefix);
+	KEYBUFFPREFIX(b3, k3, prefix);
 
-  sha256sse_1B(b0, b1, b2, b3, sh0, sh1, sh2, sh3);
-  ripemd160sse_32(sh0, sh1, sh2, sh3, h0, h1, h2, h3);
+	sha256sse_1B(b0, b1, b2, b3, sh0, sh1, sh2, sh3);
+	ripemd160sse_32(sh0, sh1, sh2, sh3, h0, h1, h2, h3);
 }
 
 /** ***************************************************************************
